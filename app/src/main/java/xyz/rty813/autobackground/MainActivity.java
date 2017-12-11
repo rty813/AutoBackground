03273707907fe86a600f64fd0afa1b3448c35b00
zhang <523213189@qq.com>
2017-12-11 23:22:55 +0800

    init commit

---
+++ app/src/main/java/xyz/rty813/autobackground/MainActivity.java
@@ -0,0 +1,19 @@
+package xyz.rty813.autobackground;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.support.v7.widget.Toolbar;
+import android.view.View;
+
+import pictureremind.rty813.xyz.autobackground.AutoBackground;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        Toolbar toolbar = findViewById(R.id.toolbar);
+        new AutoBackground(this, toolbar).start();
+    }
+}
